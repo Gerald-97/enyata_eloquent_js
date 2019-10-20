a3dc7df450720f7ea99cf9e6d52a6ebc978ee5cb
Gerald <geraldinyiama@gmail.com>
2019-10-20 13:30:07 +0100

    New additions

---
+++ Higher_Order_Functions/exercise.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Question 1
+ * 
+ */
+var arrOfArray = [2, 
+    [1,],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 
+        [12], 
+        [1,2,
+            [5,7,
+                [9]
+            ]
+        ]
+    ]
+];
+// var arrOfArray = [1,2,3,[4,5,[6,7,[8,9,[10,11,[12]]]]]];
+
+function flattener(arr1, arr2){
+    flatArr =[];
+    flatArr +=  arr1.concat(arr2);
+    return flatArr;
+};
+var arrOfArray1 = arrOfArray.reduce(flattener);
+console.log(arrOfArray1);
+console.log('-----------------------------------')
+/**
+ * 
+ * Question 3
+ * 
+ */
